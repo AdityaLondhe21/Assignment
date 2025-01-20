d22100ce75aba62bdbc9da627991142402fd382b
AdityaLondhe21 <aditya.londhe@wissen.com>
2025-01-20 12:41:39 +0530

    Synchronization so that all racers start same
    No of racers and distance are taken form users

--- Assignment7/src/gameapplication/Biker.java
+++ Assignment7/src/gameapplication/Biker.java
@@ -1,38 +1,56 @@
 package gameapplication;
 
-public class Biker implements Runnable{
-	private String name;
-	private long startTime;
-	private long endTime;
-	private long timeTaken ;
-	public Biker(String name) {
-		this.name = name;
-	}
-	public String getName() {
+public class Biker implements Runnable {
+    private String name;
+    private long startTime;
+    private long endTime;
+    private long timeTaken;
+    public static boolean gameStarted;
+    static Object lock = new Object();
+    public int progress;
+
+    public Biker(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
         return this.name;
     }
+
     public long getStartTime() {
         return this.startTime;
     }
+
     public long getEndTime() {
         return this.endTime;
     }
+
     public long getTimeTaken() {
         return this.timeTaken;
     }
+
     public void run() {
+        synchronized (lock) {
+            while (!gameStarted) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         this.startTime = System.currentTimeMillis();
-        
-        for (int i = 1; i <= 100; i++) {
+
+        for (int i = 1; i <= RacingDetails.distance; i++) {
             try {
-                Thread.sleep(20 * ((int) (Math.random()*10) % 10));
+                Thread.sleep((int) (Math.random() * 100));
+                progress = i;
             } catch (Exception e) {
                 System.out.println(e);
             }
         }
         this.endTime = System.currentTimeMillis();
-        
         this.timeTaken = endTime - startTime;
     }
-
 }
--- Assignment7/src/gameapplication/RacingGame.java
+++ Assignment7/src/gameapplication/RacingGame.java
@@ -1,43 +1,74 @@
 package gameapplication;
 
+import java.util.Scanner;
+
 public class RacingGame {
 
-    private Biker[] bikers = new Biker[10];
+    private Biker[] bikers;
 
-    public RacingGame() {
-        for (int i = 0; i < 10; i++) {
-            bikers[i] = new Biker("Biker " + (i+1));
+    public RacingGame(int noOfBikers) {
+        bikers = new Biker[noOfBikers];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < noOfBikers; i++) {
+            System.out.print("Enter the name of Biker " + (i + 1) + ": ");
+            String name = scanner.next();
+            bikers[i] = new Biker(name);
         }
     }
 
     private void displayResults() {
         System.out.println("Race Results: (Time in milliseconds)");
-        for (int i = 0; i < 10; i++) {
+        System.out.println("Rank\tBiker Name\tStart Time\tEnd Time\tTotal Time");
+        for (int i = 0; i < bikers.length; i++) {
             Biker biker = bikers[i];
-            System.out.println((i+1)+" : "+biker.getName()+
-            		" ->\t Start Time : "+ biker.getStartTime()+
-            		" ,End Time : "+ biker.getEndTime()+
-            		" ,Time Taken : "+ biker.getTimeTaken());       
+            System.out.printf("%d\t%s\t\t%d\t%d\t%d%n",
+                    i + 1, biker.getName(), biker.getStartTime(), biker.getEndTime(), biker.getTimeTaken());
         }
     }
+
     public void startRace() throws Exception {
         System.out.println("Race starting in:");
         for (int i = 10; i > 0; i--) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
             Thread.sleep(1000);
         }
         System.out.println("Go!");
 
-        Thread[] threads = new Thread[10];
-        for (int i=0 ; i<10 ;i++) {
+        Thread[] threads = new Thread[bikers.length];
+        for (int i = 0; i < bikers.length; i++) {
             threads[i] = new Thread(bikers[i]);
             threads[i].start();
         }
+        synchronized (Biker.lock) {
+            Biker.gameStarted = true;
+            Biker.lock.notifyAll();
+        }
+
+        boolean anyThreadAlive;
+        do {
+            anyThreadAlive = false;
+            for (Thread thread : threads) {
+                if (thread.isAlive()) {
+                    anyThreadAlive = true;
+                    break;
+                }
+            }
+            System.out.println("----------------------------------------------------------------------------------------------");
+            for (int i = 0; i < bikers.length; i++) {
+                System.out.print(bikers[i].getName() + ": ");
+                for (int j = 0; j < bikers[i].progress; j++) {
+                    System.out.print("_");
+                }
+                System.out.println("o-`o");
+            }
+            System.out.println("----------------------------------------------------------------------------------------------");
+            Thread.sleep(1000);
+        } while (anyThreadAlive);
 
         for (Thread thread : threads) {
             thread.join();
         }
-        //bubble sort
+
         for (int i = 0; i < bikers.length - 1; i++) {
             for (int j = 0; j < bikers.length - i - 1; j++) {
                 if (bikers[j].getEndTime() > bikers[j + 1].getEndTime()) {
@@ -49,15 +80,14 @@ public class RacingGame {
         }
         displayResults();
     }
-  
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
         try {
-            RacingGame race = new RacingGame();
+            RacingDetails details = new RacingDetails();
+            RacingGame race = new RacingGame(RacingDetails.noOfBikers);
             race.startRace();
-            
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-
 }
